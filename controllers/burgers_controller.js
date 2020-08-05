4393e2eedcd815285db1ffe1ceb10fd25d85633e
nrmarsh36 <nrmarsh36@gmail.com>
2020-08-05 15:08:00 -0500

    main and index pages mostly set up

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const burger = require("../models/burger.js");
+
+const router = express.Router();
+
+router.get() {
+
+}
+
+router.post() {
+
+}
+
+router.put() {
+
+}
+
+module.exports = router;
